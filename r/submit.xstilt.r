5788e4d707ae1614522f51616f000d7336593a38
Dustin Roten <u1211790@kingspeak4.wasatch.peaks>
2020-10-16 14:14:02 -0600

    Added a conditional statement for modeled vs observational data

--- r/submit.xstilt.r
+++ r/submit.xstilt.r
@@ -1,7 +1,10 @@
 submit.xstilt <- function(input.variables = NULL, user.id = NULL) {
   
+  mode <- unique(input.variables$oco.sensor)
+  if(length(mode) != 1) return(NULL)
+  
   # Input variables for OCO-2 and OCO-3 modeling
-  if(input.variables$oco.sensor != 'Modeled') {
+  if(mode != 'Modeled') {
 
     # Prepare to loop through each submitted job.
     for(i in 1:nrow(input.variables)) {
@@ -24,30 +27,31 @@ submit.xstilt <- function(input.variables = NULL, user.id = NULL) {
     }
     
     # Input variables for modeled XCO2 over custom domains.
-  } else if(oco.sensor == 'Modeled') {
+  } else if(mode == 'Modeled') {
     
-    for(i in 1:nrow(input.variables)) {
-
-      #' monitor parallel jobs. A new job will not be submitted until the
-      #' previous one is completed.
-      user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
-      msg.flag <- 0 # msg.flag == 0 will display a message when jobs are running.
-
-      # Hang out while previous jobs are running.
-      while(nrow(user.jobs) > 1) {
-        if(msg.flag == 0) message('User has jobs in progress. Please wait.')
-        Sys.sleep(300); msg.flag <- 1
-        user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
-      }; msg.flag <- 0
-
-      # Submit the job (interpolation)
-      run_xstilt_UrBAnFlux_2(input.variables = input.variables[i,])
-      message(paste0('Custom domain job ', i, ' of ', nrow(input.variables), ' submitted.'))
-    }
+    #' for(i in 1:nrow(input.variables)) {
+    #' 
+    #'   #' monitor parallel jobs. A new job will not be submitted until the
+    #'   #' previous one is completed.
+    #'   user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
+    #'   msg.flag <- 0 # msg.flag == 0 will display a message when jobs are running.
+    #' 
+    #'   # Hang out while previous jobs are running.
+    #'   while(nrow(user.jobs) > 1) {
+    #'     if(msg.flag == 0) message('User has jobs in progress. Please wait.')
+    #'     Sys.sleep(300); msg.flag <- 1
+    #'     user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
+    #'   }; msg.flag <- 0
+    #' 
+    #'   # Submit the job (interpolation)
+    #'   run_xstilt_UrBAnFlux_2(input.variables = input.variables[i,])
+    #'   message(paste0('Custom domain job ', i, ' of ', nrow(input.variables), ' submitted.'))
+    #' }
     
     # Start interpolation scheme here
     for(i in 1:nrow(input.variables)) {
-      interpolate_UrBAnFlux(input.variables)
+      message(paste0('Interpolating job ', i, '.'))
+      interpolate_UrBAnFlux(input.variables[i,])
     }
     
   }
