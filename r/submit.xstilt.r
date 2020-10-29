c6f30b4d23644b04f25dd7dc311399fe40c643ad
Dustin Roten <u1211790@kingspeak4.wasatch.peaks>
2020-10-29 16:57:59 -0600

    Expanded the conditional statements to include interpolation scheme

--- r/submit.xstilt.r
+++ r/submit.xstilt.r
@@ -1,24 +1,26 @@
-submit.xstilt <- function(input.variables = NULL, user.id = NULL) {
+submit.xstilt <- function(input.variables = NULL) {
   
   mode <- unique(input.variables$oco.sensor)
-  if(length(mode) != 1) return(NULL)
+  if(length(mode) != 1) stop('Multiple modes submitted')
   
   # Input variables for OCO-2 and OCO-3 modeling
-  if(mode != 'Modeled') {
+  if(mode != 'Modeled' & mode != 'Interpolate') {
 
     # Prepare to loop through each submitted job.
     for(i in 1:nrow(input.variables)) {
       
       #' monitor parallel jobs. A new job will not be submitted until the
       #' previous one is completed.
-      user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
+      user.jobs <- subset(SLURM.jobs(partition = input.variables$partition[i]),
+                          USER == input.variables$user.id[i])
       msg.flag <- 0 # msg.flag == 0 will display a message when jobs are running.
       
       # Hang out while previous jobs are running.
       while(nrow(user.jobs) > 1) {
         if(msg.flag == 0) message('User has jobs in progress. Please wait.')
         Sys.sleep(300); msg.flag <- 1
-        user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
+        user.jobs <- subset(SLURM.jobs(partition = input.variables$partition[i]),
+                            USER == input.variables$user.id[i])
       }; msg.flag <- 0
       
       # Submit the job
@@ -27,32 +29,40 @@ submit.xstilt <- function(input.variables = NULL, user.id = NULL) {
     }
     
     # Input variables for modeled XCO2 over custom domains.
-  } else if(mode == 'Modeled') {
+  } else if(mode == 'Modeled' | mode == 'Interpolate') {
     
-    #' for(i in 1:nrow(input.variables)) {
-    #' 
-    #'   #' monitor parallel jobs. A new job will not be submitted until the
-    #'   #' previous one is completed.
-    #'   user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
-    #'   msg.flag <- 0 # msg.flag == 0 will display a message when jobs are running.
-    #' 
-    #'   # Hang out while previous jobs are running.
-    #'   while(nrow(user.jobs) > 1) {
-    #'     if(msg.flag == 0) message('User has jobs in progress. Please wait.')
-    #'     Sys.sleep(300); msg.flag <- 1
-    #'     user.jobs <- subset(SLURM.jobs(partition = 'lin-kp'), USER == user.id)
-    #'   }; msg.flag <- 0
-    #' 
-    #'   # Submit the job (interpolation)
-    #'   run_xstilt_UrBAnFlux_2(input.variables = input.variables[i,])
-    #'   message(paste0('Custom domain job ', i, ' of ', nrow(input.variables), ' submitted.'))
-    #' }
+    if(mode == 'Modeled') {
     
-    # Start interpolation scheme here
-    for(i in 1:nrow(input.variables)) {
-      message(paste0('Interpolating job ', i, '.'))
-      interpolate_UrBAnFlux(input.variables[i,])
-    }
+      for(i in 1:nrow(input.variables)) {
+        #' monitor parallel jobs. A new job will not be submitted until the
+        #' previous one is completed.
+        user.jobs <- subset(SLURM.jobs(partition = input.variables$partition[i]),
+                            USER == input.variables$user.id[i])
+        msg.flag <- 0 # msg.flag == 0 will display a message when jobs are running.
+        
+        # Hang out while previous jobs are running.
+        while(nrow(user.jobs) > 1 | i == 2) {
+          if(msg.flag == 0) message('User has jobs in progress. Please wait.')
+          Sys.sleep(300); msg.flag <- 1
+          user.jobs <- subset(SLURM.jobs(partition = input.variables$partition[i]),
+                              USER == input.variables$user.id[i])
+        }; msg.flag <- 0
+        
+        # Submit the job (interpolation)
+        run_xstilt_UrBAnFlux_2(input.variables = input.variables[i,])
+        message(paste0('Custom domain job ', i, ' of ', nrow(input.variables), ' submitted.'))
+        
+        #' To save space, remove the unwanted files from the previous directory
+        if(i > 1) {purge.directories(directory = input.variables$store.path,
+                                     iteration = (i-1))}
+      }
+      
+      # One final check after the runs are complete. Unused files are removed.
+      purge.directories(directory = input.variables$store.path, iteration = 0)
+      
+    } #closes if statement for interpolation
+    
+    interpolate_apply(f = interpolate_UrBAnFlux, input.variables)
     
   }
 }
