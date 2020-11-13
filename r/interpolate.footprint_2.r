5c0f59dc557e9a74453417a7f64ed81959903985
Dustin Roten <u1211790@kingspeak4.wasatch.peaks>
2020-11-13 13:26:36 -0700

    Significant overhaul. empty rasters needed resolution assignment.

--- r/interpolate.footprint_2.r
+++ r/interpolate.footprint_2.r
@@ -1,15 +1,34 @@
 #' `intepolate.footprint_1` is used to interpolate footprints that are already
 #' time-integrated by the X-STILT model. To interpolate footprints that are NOT
 #' time-integrated, use `interpolate.footprint_2`.
-interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.receptor = NULL,
-                                    receptor.resolution = NULL, output.directory = NULL,
-                                    footprint.directory = NULL, time_integrate = NULL) {
+interpolate.footprint_2 <- function(receptor.resolution, workdir,
+                                    x.idx, y.idx, interp.lon, interp.lat, interp.filepath,
+                                    x.idx1, y.idx1, vertex1.lon, vertex1.lat, vertex1.file.name,
+                                    x.idx2, y.idx2, vertex2.lon, vertex2.lat, vertex2.file.name,
+                                    x.idx3, y.idx3, vertex3.lon, vertex3.lat, vertex3.file.name,
+                                    x.idx4, y.idx4, vertex4.lon, vertex4.lat, vertex4.file.name) {
+  
+  setwd(workdir); source('r/dependencies.r')
+  options(stringsAsFactors = FALSE)
+  
+  interpolated.receptor <- data.frame(x.idx, y.idx, interp.lon, interp.lat)
+  interpolated.receptor$interp.filepath <- interp.filepath
+  names(interpolated.receptor) <- c('x.idx', 'y.idx', 'long', 'lati', 'interp.filepath')
+  
+  xstilt.receptors <- data.frame(
+    rbind(x.idx1, x.idx2, x.idx3, x.idx4),
+    rbind(y.idx1, y.idx2, y.idx3, y.idx4),
+    rbind(vertex1.lon, vertex2.lon, vertex3.lon, vertex4.lon),
+    rbind(vertex1.lat, vertex2.lat, vertex3.lat, vertex4.lat),
+    rbind(vertex1.file.name, vertex2.file.name, vertex3.file.name, vertex4.file.name),
+    row.names = NULL
+  ); names(xstilt.receptors) <- c('x.idx', 'y.idx', 'long', 'lati', 'vertex.file.name') 
   
   # Add in the weighted footprints from xstilt-generated footprints.
   # First, determine the number of footprints required.
   inline.receptors <- subset(xstilt.receptors,
                              x.idx == interpolated.receptor$x.idx | y.idx == interpolated.receptor$y.idx)
-  
+ 
   #' If the receptor location to be interpolated is in-line with xstilt receptors,
   #' use only the two closest column-receptors and their associated footprints to
   #' perform the interpolation. These tend to be more accurate than the interior
@@ -17,14 +36,15 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
   if(nrow(inline.receptors) != 0) {
     distance.list <- NULL
     for(i in 1:nrow(inline.receptors)) {
+      
       # first, calculate the distance from the xstilt receptor to the interpolated location.
       # Add this distance to the "total.distance" to use for normalization later.
       distance <- distHaversine(p1 = c(inline.receptors$long[i], inline.receptors$lati[i]),
                                 p2 = c(interpolated.receptor$long, interpolated.receptor$lati))
       distance.list[i] <- abs(distance)
-      
+
       # read in the footprint and shift if accordingly.
-      footprint <- stack(inline.receptors$file.path[i])
+      footprint <- stack(inline.receptors$vertex.file.name[i])
       
       # determine increments to shift by.
       delta.x <- receptor.resolution*(interpolated.receptor$x.idx - inline.receptors$x.idx[i])
@@ -40,7 +60,7 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       # save shifted footprint to a generic variable
       eval(parse(text = paste0('foot.', i, ' <- footprint')))
     }
-    
+
     ##### Interpolate each layer here (2 footprints used) #####
     #' Collect all of the raster layer names here.
     #' Next, begin interpolating each layer.
@@ -52,7 +72,8 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       foot.1.layer <- tryCatch({
         eval(parse(text = paste0('foot.1.layer <- foot.1$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.1); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.1); res(r) <- res(foot.1)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -61,7 +82,8 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       foot.2.layer <- tryCatch({
         eval(parse(text = paste0('foot.2.layer <- foot.2$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.2); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.2); res(r) <- res(foot.2)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -81,6 +103,7 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     ##### End of layer interpolation (2) #####
     
     # create the interpolated footprint's directory and create its filename
+    footprint.directory <- interpolated.receptor$interp.filepath
     suppressWarnings(dir.create(footprint.directory, recursive = TRUE))
     output.filename <- file.path(footprint.directory,
                                  paste0(basename(footprint.directory), '_foot.nc'))
@@ -88,7 +111,10 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     
     # Snap the interpolated footprint to the common grid.
     interpolated.footprint <- resample(interpolated.footprint, foot.1,
-                                       method = 'ngb', filename = output.filename)
+                                       method = 'ngb',
+                                       filename = file.path(footprint.directory,
+                                                            'tmp.file.nc'),
+                                       overwrite = TRUE)
     
     #' The current process needs to be replaced with something more clever.
     #' The previous line saves the appropriate file in the appropriate location.
@@ -100,12 +126,20 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     # Now, the file is saved properly in a format similar to the other footprints.
     write_interp.footprint(footprint.raster = interpolated.footprint, output.filename)
     
+    # Get rid of the overly obnoxius temp file
+    if(file.exists(output.filename))
+      file.remove(file.path(footprint.directory, 'tmp.file.nc'))
+    
     # add a symbolic link in the "footprints" folder for easy access
     # Symlink footprint to out/footprints
+    # CAREFUL! output.directory is a relative file path
+    output.directory <- dirname(dirname(footprint.directory))
     link <- file.path(output.directory, 'footprints', basename(output.filename))
     suppressWarnings(file.symlink(output.filename, link))
   }
   
+  
+  ####################################################################################
   #' If the receptor location to be interpolated is NOT in-line with xstilt receptors,
   #' use all four of the column-receptors and their associated footprints to
   #' perform the interpolation.
@@ -119,7 +153,7 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       distance.list[i] <- abs(distance)
       
       # read in the footprint and shift if accordingly.
-      footprint <- stack(xstilt.receptors$file.path[i])
+      footprint <- stack(xstilt.receptors$vertex.file.name[i])
       
       # determine increments to shift by.
       delta.x <- receptor.resolution*(interpolated.receptor$x.idx - xstilt.receptors$x.idx[i])
@@ -139,13 +173,14 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     #' Next, begin interpolating each layer.
     layer.names <- unique(c(names(foot.1), names(foot.2), names(foot.3), names(foot.4)))
     for(i in 1:length(layer.names)) {
-      
+
       #' Grab each layer of footprint 1
       #' If a layer is missing, make one with all zeros
       foot.1.layer <- tryCatch({
         eval(parse(text = paste0('foot.1.layer <- foot.1$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.1); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.1); res(r) <- res(foot.1)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -154,7 +189,8 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       foot.2.layer <- tryCatch({
         eval(parse(text = paste0('foot.2.layer <- foot.2$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.2); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.2); res(r) <- res(foot.2)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -163,7 +199,8 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       foot.3.layer <- tryCatch({
         eval(parse(text = paste0('foot.3.layer <- foot.3$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.3); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.3); res(r) <- res(foot.3)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -172,7 +209,8 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
       foot.4.layer <- tryCatch({
         eval(parse(text = paste0('foot.4.layer <- foot.4$', layer.names[i])))
       }, error=function(cond){
-        r <- raster(); extent(r) <- extent(foot.4); values(r) <- 0
+        r <- raster(); extent(r) <- extent(foot.4); res(r) <- res(foot.4)
+        values(r) <- 0
         return(r)
       }, finally = {})
       
@@ -195,6 +233,7 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     ##### End of layer interpolation (4) #####
     
     # create the interpolated footprint's directory and create its filename
+    footprint.directory <- interpolated.receptor$interp.filepath
     suppressWarnings(dir.create(footprint.directory, recursive = TRUE))
     output.filename <- file.path(footprint.directory,
                                  paste0(basename(footprint.directory), '_foot.nc'))
@@ -202,7 +241,10 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     
     # Snap the interpolated footprint to the common grid.
     interpolated.footprint <- resample(interpolated.footprint, foot.1,
-                                       method = 'ngb', filename = output.filename)
+                                       method = 'ngb',
+                                       filename = file.path(footprint.directory,
+                                                            'tmp.file.nc'),
+                                       overwrite = TRUE)
     
     #' The current process needs to be replaced with something more clever.
     #' The previous line saves the appropriate file in the appropriate location.
@@ -214,10 +256,16 @@ interpolate.footprint_2 <- function(xstilt.receptors = NULL, interpolated.recept
     # Now, the file is saved properly in a format similar to the other footprints.
     write_interp.footprint(footprint.raster = interpolated.footprint, output.filename)
     
+    # Get rid of the overly obnoxius temp file
+    if(file.exists(output.filename))
+      file.remove(file.path(footprint.directory, 'tmp.file.nc'))
+    
     # add a symbolic link in the "footprints" folder for easy access
     # Symlink footprint to out/footprints
+    # CAREFUL! output.directory is a relative file path
+    output.directory <- dirname(dirname(footprint.directory))
     link <- file.path(output.directory, 'footprints', basename(output.filename))
     suppressWarnings(file.symlink(output.filename, link))
-  }
-  
+    
+  } #closes if inline.receptors == FALSE
 }
