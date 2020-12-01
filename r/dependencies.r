f2fe2ac39112887320fd84caa29acdf247a30125
Dustin Roten <u1211790@kingspeak4.wasatch.peaks>
2020-12-01 11:56:59 -0700

    minor directory change

--- r/dependencies.r
+++ r/dependencies.r
@@ -1,13 +1,16 @@
 ### Source R scripts for Dien's X-STILT, 05/23/2018, DW
-rsc <- dir('r', pattern = '.*\\.r$',
+rsc <- dir('r/src', pattern = '.*\\.r$',
            full.names = T, recursive = T)
-rsc <- subset(rsc, rsc != 'r/dependencies.r')
 
 invisible(lapply(rsc, source))
 
 # Other relevant packages
 library(geosphere); library(raster); library(dplyr)
-library(ncdf4)
+library(ncdf4); library(lubridate); library(stringr)
+library(ggmap)
 
 # required STILT functions
 source('X-STILT/stilt_hysplit/r/src/write_footprint.r')
+
+# required X-STILT functions
+source('X-STILT/r/src/get.lon.lat.r')
