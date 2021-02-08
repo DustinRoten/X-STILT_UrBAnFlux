b73164c1898ff825a0138252cd14e51da568cadb
Dustin Roten <u1211790@kingspeak4.wasatch.peaks>
2021-02-08 10:13:50 -0700

    clean up

--- r/src/convolution_apply.r
+++ r/src/convolution_apply.r
@@ -1,57 +1,66 @@
-convolution_apply <- function(site = NULL, footprint.directory = NULL, edgar.directory = NULL,
-                              temporal.directory = NULL,odiac.directory = NULL, carma.filepath = NULL,
-                              work.dir = NULL, slurm_options = NULL, user.id = NULL) {
+#convolution_apply
+convolution_apply <- function(user.id, homedir, site, lon.lat,
+                              urban.domain, available.directories,
+                              slurm_options, sector.list, edgar.inventory,
+                              odiac.inventory, smurf.inventory,
+                              carma.inventory, temporal.downscaling,
+                              output.path) {
   
-  output.directory <- list.files(footprint.directory, pattern = gsub(' ', '', site),
-                                 full.names = TRUE)
-  
-  # Create output XCO2 directories
-  lapply(file.path(output.directory, 'XCO2'), 'dir.create',
-         showWarnings = FALSE, recursive = TRUE)
-  
-  #' Use the location string to obtain geographic information.
-  lon.lat <- geocode(site)
-  time.zone <-
-    suppressWarnings(tz_lookup_coords(lat = lon.lat$lat, lon = lon.lat$lon))
-  
-  for(i in 1:length(output.directory)) {
+  #check that the timestrs in each directory are consistent.
+  for(i in 1:length(available.directories)) {
+    timestr.list <- c(str_split_fixed(list.files(available.directories[i]),
+                                      pattern = '_', n = 5)[,3])
     
-    footprint.list <- list.files(file.path(output.directory[i], 'footprints'),
-                                 full.names = TRUE)
-  
-    job.list <- data.frame(
-      site = site,
-      local.tz = time.zone,
-      citylon = lon.lat$lon,
-      citylat = lon.lat$lat,
-      work.dir = getwd(),
-      temp.dir = temporal.directory,
-      odiac.dir = odiac.directory,
-      edgar.dir = edgar.directory,
-      carma.file = carma.filepath,
-      out.dir = file.path(output.directory[i], 'XCO2'),
-      footprint = footprint.list
-    )
-    
-    SLURM.wait(selected.partition = slurm_options$partition,
-               user.id = user.id)
-    
-    sjob <- rslurm::slurm_apply(f = convolve.FullFlux,
-                                job.list,
-                                jobname = 'Convolution',
-                                pkgs = 'base',
-                                nodes = 6,
-                                cpus_per_node = 10,
-                                slurm_options = slurm_options)
-    
-    message(paste0('Job ', i, ' of ', length(output.directory),
-                   ' submitted.'))
-    
-    notify(user.email = '3366202544@cwwsms.com',
-           subject.text = 'CHCP - Kingspeak Jobs',
-           body.text = paste0('Job ', i, ' of ', length(output.directory),
-                              ' submitted.'))
-    
+    if(i > 1) {
+      check <- all(timestr.list == timestr.list.previous)
+    } else {check <- TRUE}
+    if(!check) stop('Timestr mismatch in directories')
+    timestr.list.previous <- timestr.list
   }
+  
+  timestr.list <- c(str_split_fixed(list.files(available.directories),
+                                    pattern = '_', n = 5)[,3])
+  timestr.list <- unique(timestr.list)
+  
+  #what directories are present?
+  edgar.idx <-
+    which(!is.na(str_match(available.directories, pattern = 'EDGAR')))
+  if(length(edgar.idx) != 0) {
+    EDGAR.directory <- available.directories[edgar.idx]
+  } else {EDGAR.directory <- NA}
+  
+  odiac.idx <-
+    which(!is.na(str_match(available.directories, pattern = 'ODIAC')))
+  if(length(odiac.idx) != 0) {
+    ODIAC.directory <- available.directories[odiac.idx]
+  } else {ODIAC.directory <- NA}
+  
+  smurf.idx <-
+    which(!is.na(str_match(available.directories, pattern = 'SMUrF')))
+  if(length(smurf.idx) != 0) {
+    SMUrF.directory <- available.directories[smurf.idx]
+  } else {SMUrF.directory <- NA}
+  
+  
+  for(i in 1:length(timestr))
+  
+  #construct the dataframe of SLURM jobs
+  job.list <- data.frame(homedir, site, timestr = timestr.list,
+                         lon.lat, urban.domain,
+                         EDGAR.directory, ODIAC.directory,
+                         SMUrF.directory, sector.list,
+                         edgar.inventory, odiac.inventory,
+                         smurf.inventory, carma.inventory,
+                         temporal.downscaling, output.path)
+  
+  nodes <- ceiling(nrow(job.list)/8)
+  
+  sjob <- rslurm::slurm_apply(f = convolve.FullFlux,
+                              job.list,
+                              jobname = 'FullFlux',
+                              pkgs = 'base',
+                              nodes = nodes,
+                              cpus_per_node = 12,
+                              slurm_options = slurm_options)
   
 }
